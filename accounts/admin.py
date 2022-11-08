745cb824ea346491fc00e1931d3c9ae4b05b3b44
X00187555 Hanley Josh <x00187555@TA.IT-TALLAGHT.IE>
2022-11-08 11:41:57 +0000

    lab 9 Part 1 commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+from .models import Profile
 
 
 # Register your models here.
